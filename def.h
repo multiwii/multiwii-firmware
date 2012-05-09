79be5c1612eb8c4e0f3b8f9429866da9bfaad6d3
Felix.Niessen@gmail.com <Felix.Niessen@gmail.com@02679b44-d973-9852-f2fa-63770883b36c>
2012-05-09 14:57:31 +0000

    define typo fix
    git-svn-id: http://multiwii.googlecode.com/svn/trunk/MultiWii_shared@768 02679b44-d973-9852-f2fa-63770883b36c

--- def.h
+++ def.h
@@ -5,7 +5,7 @@
 #if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__)
   #define PROMINI
 #endif
-#if defined(__AVR_ATmega32U4__) || #defined(TEENSY20)
+#if defined(__AVR_ATmega32U4__) || defined(TEENSY20)
   #define PROMICRO
 #endif
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega1281__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega2561__)
