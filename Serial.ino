5d24bb5b4d5aa7f996560b17625ab9096413dc8f
stefan@pico.ruhr.de <stefan@pico.ruhr.de@02679b44-d973-9852-f2fa-63770883b36c>
2012-06-08 10:28:12 +0000

    rewrite serial com state machine and protocol
    git-svn-id: http://multiwii.googlecode.com/svn/trunk/MultiWii_shared@860 02679b44-d973-9852-f2fa-63770883b36c

--- Serial.ino
+++ Serial.ino
@@ -39,8 +39,8 @@
 
 #define MSP_DEBUG                254   //out message         debug1,debug2,debug3,debug4
 
-
-static uint8_t checksum,stateMSP,indRX,inBuf[64];
+#define INBUF_SIZE 64
+static uint8_t checksum,indRX,inBuf[INBUF_SIZE];
 
 uint32_t read32() {
   uint32_t t = inBuf[indRX++];
@@ -58,7 +58,13 @@ uint16_t read16() {
 uint8_t read8()  {return inBuf[indRX++]&0xff;}
 
 void headSerialReply(uint8_t c,uint8_t s) {
-  serialize8('$');serialize8('M');serialize8('>');serialize8(s);serialize8(c);checksum = 0;
+  serialize8('$');
+  serialize8('M');
+  serialize8('>');
+  /* start calculating a new checksum */
+  checksum = 0;
+  serialize8(s);
+  serialize8(c);
 }
 
 void tailSerialReply() {
@@ -66,229 +72,191 @@ void tailSerialReply() {
 }
 
 void serialCom() {
-  uint8_t i, c;
-  static uint8_t offset,dataSize;
+  uint8_t c;
+  static uint8_t cmd;
+  static uint8_t offset;
+  static uint8_t dataSize;
+  static enum _serial_state {
+    IDLE,
+    HEADER_START,
+    HEADER_M,
+    HEADER_ARROW,
+    HEADER_SIZE,
+    HEADER_CMD,
+  } c_state = IDLE;
   
   while (SerialAvailable(0)) {
     c = SerialRead(0);
 
-    if (stateMSP > 99) {                           // a message with a length indication, indicating a non null payload
-      if (offset < dataSize) {                     // there are still some octets to read (including checksum) to complete a full message
-        checksum ^= c;     						   // the checksum is computed, except for the last octet
-        inBuf[offset++] = c;
-      } else {                                     // we have read all the payload
-        if ( checksum == c ) {                     // we check is the computed checksum is ok
-          switch(stateMSP) {                       // if yes, then we execute different code depending on the message code. read8/16/32 will look into the inBuf buffer
-            case MSP_SET_RAW_RC:
-              for(i=0;i<8;i++) {rcData[i] = read16();} break;
-            case MSP_SET_RAW_GPS:
-              GPS_fix = read8();
-              GPS_numSat = read8();
-              GPS_coord[LAT] = read32();
-              GPS_coord[LON] = read32();
-              GPS_altitude = read16();
-              GPS_speed = read16();
-              GPS_update = 1; break;
-            case MSP_SET_PID:
-              for(i=0;i<PIDITEMS;i++) {conf.P8[i]=read8();conf.I8[i]=read8();conf.D8[i]=read8();} break;
-            case MSP_SET_BOX:
-              for(i=0;i<CHECKBOXITEMS;i++) {conf.activate[i]=read16();} break;
-            case MSP_SET_RC_TUNING:
-              conf.rcRate8 = read8();
-              conf.rcExpo8 = read8();
-              conf.rollPitchRate = read8();
-              conf.yawRate = read8();
-              conf.dynThrPID = read8();
-              conf.thrMid8 = read8();
-              conf.thrExpo8 = read8();break;
-            case MSP_SET_MISC:
-              #if defined(POWERMETER)
-                conf.powerTrigger1 = read16() / PLEVELSCALE; // we rely on writeParams() to compute corresponding pAlarm value
-              #endif
-              break;
-          }
-        }
-        stateMSP = 0;                              // in any case we reset the MSP state
+    if (c_state == IDLE) {
+      c_state = (c=='$') ? HEADER_START : IDLE;
+    } else if (c_state == HEADER_START) {
+      c_state = (c=='M') ? HEADER_M : IDLE;
+    } else if (c_state == HEADER_M) {
+      c_state = (c=='<') ? HEADER_ARROW : IDLE;
+    } else if (c_state == HEADER_ARROW) {
+      /* now we are expecting the payload size */
+      if (c > INBUF_SIZE) {
+        c_state = IDLE;
+        continue;
       }
-    }
-
-    if (stateMSP < 5) {
-      if (stateMSP == 4) {                           // this protocol state indicates we have a message with a lenght indication, and we read here the message code (fifth octet) 
-        if (c > 99) {                                // we check if it's a valid code (should be >99)
-          stateMSP = c;                              // the message code is then reuse to feed the protocol state
-          offset = 0;checksum = 0;indRX=0;           // and we init some values which will be used in the next loops to grasp the payload
-        } else {
-          stateMSP = 0;                              // the message code seems to be invalid. this should not happen => we reset the protocol state
-        }
-      }
-      if (stateMSP == 3) {                           // here, we need to check if the fourth octet indicates a code indication (>99) or a payload lenght indication (<100)
-        if (c<100) {                                 // a message with a length indication, indicating a non null payload
-          stateMSP++;                                // we update the protocol state to read the next octet
-          dataSize = c;                              // we store the payload lenght
-          if (dataSize>63) dataSize=63;              // in order to avoid overflow, we limit the size. this should not happen
-        } else {
-          switch(c) {                                // if we are here, the fourth octet indicates a code message
-            case MSP_IDENT:                          // and we check message code to execute the relative code
-              headSerialReply(c,2);                  // we reply with an header indicating a payload lenght of 2 octets
-              serialize8(VERSION);                   // the first octet. serialize8/16/32 is used also to compute a checksum
-              serialize8(MULTITYPE);                 // the second one
-              tailSerialReply();break;               // mainly to send the last octet which is the checksum
-            case MSP_STATUS:
-              headSerialReply(c,8);
-              serialize16(cycleTime);
-              serialize16(i2c_errors_count);
-              serialize16(ACC|BARO<<1|MAG<<2|GPS<<3|SONAR<<4);
-              serialize16(accMode<<BOXACC|baroMode<<BOXBARO|magMode<<BOXMAG|armed<<BOXARM|
-                          GPSModeHome<<BOXGPSHOME|GPSModeHold<<BOXGPSHOLD|headFreeMode<<BOXHEADFREE|
-                          passThruMode<<BOXPASSTHRU|rcOptions[BOXBEEPERON]<<BOXBEEPERON);
-              tailSerialReply();break;
-            case MSP_RAW_IMU:
-              headSerialReply(c,18);
-              for(i=0;i<3;i++) serialize16(accSmooth[i]);
-              for(i=0;i<3;i++) serialize16(gyroData[i]);
-              for(i=0;i<3;i++) serialize16(magADC[i]);
-              tailSerialReply();break;
-            case MSP_SERVO:
-              headSerialReply(c,16);
-              for(i=0;i<8;i++) serialize16(servo[i]);
-              tailSerialReply();break;
-            case MSP_MOTOR:
-              headSerialReply(c,16);
-              for(i=0;i<8;i++) serialize16(motor[i]);
-              tailSerialReply();break;
-            case MSP_RC:
-              headSerialReply(c,16);
-              for(i=0;i<8;i++) serialize16(rcData[i]);
-              tailSerialReply();break;
-            case MSP_RAW_GPS:
-              headSerialReply(c,14);
-              serialize8(GPS_fix);
-              serialize8(GPS_numSat);
-              serialize32(GPS_coord[LAT]);
-              serialize32(GPS_coord[LON]);
-              serialize16(GPS_altitude);
-              serialize16(GPS_speed);
-              tailSerialReply();break;
-            case MSP_COMP_GPS:
-              headSerialReply(c,5);
-              serialize16(GPS_distanceToHome);
-              serialize16(GPS_directionToHome);
-              serialize8(GPS_update);
-              tailSerialReply();break;
-            case MSP_ATTITUDE:
-              headSerialReply(c,6);
-              for(i=0;i<2;i++) serialize16(angle[i]);
-              serialize16(heading);
-              tailSerialReply();break;
-            case MSP_ALTITUDE:
-              headSerialReply(c,4);
-              serialize32(EstAlt);
-              tailSerialReply();break;
-            case MSP_BAT:
-              headSerialReply(c,3);
-              serialize8(vbat);
-              serialize16(intPowerMeterSum);
-              tailSerialReply();break;
-            case MSP_RC_TUNING:
-              headSerialReply(c,7);
-              serialize8(conf.rcRate8);
-              serialize8(conf.rcExpo8);
-              serialize8(conf.rollPitchRate);
-              serialize8(conf.yawRate);
-              serialize8(conf.dynThrPID);
-              serialize8(conf.thrMid8);
-              serialize8(conf.thrExpo8);
-              tailSerialReply();break;
-            case MSP_PID:
-              headSerialReply(c,3*PIDITEMS);
-              for(i=0;i<PIDITEMS;i++)    {serialize8(conf.P8[i]);serialize8(conf.I8[i]);serialize8(conf.D8[i]);}
-              tailSerialReply();break;
-            case MSP_BOX:
-              headSerialReply(c,2*CHECKBOXITEMS);
-              for(i=0;i<CHECKBOXITEMS;i++)    {serialize16(conf.activate[i]);}
-              tailSerialReply();break;
-            case MSP_MISC:
-              headSerialReply(c,2);
-              serialize16(intPowerTrigger1);
-              tailSerialReply();break;
-            case MSP_MOTOR_PINS:
-              headSerialReply(c,8);
-              for(i=0;i<8;i++) {serialize8(PWM_PIN[i]);}
-              tailSerialReply();break;
-            case MSP_RESET_CONF:
-              conf.checkNewConf++;checkFirstTime();break;
-            case MSP_ACC_CALIBRATION:
-              calibratingA=400;break;
-            case MSP_MAG_CALIBRATION:
-              calibratingM=1;break;
-            case MSP_EEPROM_WRITE:
-              writeParams(0);break;
-            case MSP_DEBUG:
-              headSerialReply(c,8);
-              serialize16(debug1); // 4 variables are here for general monitoring purpose
-              serialize16(debug2);
-              serialize16(debug3);
-              serialize16(debug4);
-              tailSerialReply();break;
-          }
-          stateMSP=0;                              // we reset the protocol state for the next loop
-        }
+      dataSize = c;
+      offset = 0;
+      checksum = 0;
+      indRX = 0;
+      checksum ^= c;
+      /* the command is to follow */
+      c_state = HEADER_SIZE;
+    } else if (c_state == HEADER_SIZE) {
+      cmd = c;
+      checksum ^= c;
+      c_state = HEADER_CMD;
+    } else if (c_state == HEADER_CMD && offset < dataSize) {
+      checksum ^= c;
+      inBuf[offset++] = c;
+    } else if (c_state == HEADER_CMD && offset >= dataSize) {
+      /* compare calculated and transferred checksum */
+      if (checksum == c) {
+        /* we got a valid packet, evaluate it */
+        evaluateCommand(cmd, dataSize);
       } else {
-        switch(c) {                                // header detection $MW<
-          case '$':
-            if (stateMSP == 0) stateMSP++; break;  // first octet ok, no need to go further
-          case 'M':
-            if (stateMSP == 1) stateMSP++; break;  // second octet ok, no need to go further
-          case '<':
-            if (stateMSP == 2) stateMSP++; break;  // third octet ok, no need to go further
-        }
       }
-    }
-    if (stateMSP == 0) {                           // still compliant with older single octet command
-      oldSerialCom(c);
+      c_state = IDLE;
     }
   }
 }
 
-void oldSerialCom(uint8_t sr) {
-  //last things to clean
-  switch (sr) {
-    #ifdef LCD_TELEMETRY
-    case 'A': // button A press
-      toggle_telemetry(1);
-      break;
-    case 'B': // button B press
-      toggle_telemetry(2);
-      break;
-    case 'C': // button C press
-      toggle_telemetry(3);
-      break;
-    case 'D': // button D press
-      toggle_telemetry(4);
-      break;
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-    case '8':
-    case '9':
-    #if defined(LOG_VALUES) && defined(DEBUG)
-    case 'R':
-    #endif
-    #ifdef DEBUG
-    case 'F':
-    #endif
-      toggle_telemetry(sr);
-      break;
-    case 'a': // button A release
-    case 'b': // button B release
-    case 'c': // button C release
-    case 'd': // button D release
-      break;      
-    #endif // LCD_TELEMETRY
+void evaluateCommand(uint8_t c, uint8_t dataSize) {
+  switch(c) {
+   case MSP_SET_RAW_RC:
+     for(uint8_t i=0;i<8;i++) {rcData[i] = read16();} break;
+   case MSP_SET_RAW_GPS:
+     GPS_fix = read8();
+     GPS_numSat = read8();
+     GPS_coord[LAT] = read32();
+     GPS_coord[LON] = read32();
+     GPS_altitude = read16();
+     GPS_speed = read16();
+     GPS_update = 1; break;
+   case MSP_SET_PID:
+     for(uint8_t i=0;i<PIDITEMS;i++) {conf.P8[i]=read8();conf.I8[i]=read8();conf.D8[i]=read8();} break;
+   case MSP_SET_BOX:
+     for(uint8_t i=0;i<CHECKBOXITEMS;i++) {conf.activate[i]=read16();} break;
+   case MSP_SET_RC_TUNING:
+     conf.rcRate8 = read8();
+     conf.rcExpo8 = read8();
+     conf.rollPitchRate = read8();
+     conf.yawRate = read8();
+     conf.dynThrPID = read8();
+     conf.thrMid8 = read8();
+     conf.thrExpo8 = read8();break;
+   case MSP_SET_MISC:
+     #if defined(POWERMETER)
+       conf.powerTrigger1 = read16() / PLEVELSCALE; // we rely on writeParams() to compute corresponding pAlarm value
+     #endif
+     break;
+   case MSP_IDENT:                          // and we check message code to execute the relative code
+     headSerialReply(c,2);                  // we reply with an header indicating a payload lenght of 2 octets
+     serialize8(VERSION);                   // the first octet. serialize8/16/32 is used also to compute a checksum
+     serialize8(MULTITYPE);                 // the second one
+     tailSerialReply();break;               // mainly to send the last octet which is the checksum
+   case MSP_STATUS:
+     headSerialReply(c,8);
+     serialize16(cycleTime);
+     serialize16(i2c_errors_count);
+     serialize16(ACC|BARO<<1|MAG<<2|GPS<<3|SONAR<<4);
+     serialize16(accMode<<BOXACC|baroMode<<BOXBARO|magMode<<BOXMAG|armed<<BOXARM|
+                 GPSModeHome<<BOXGPSHOME|GPSModeHold<<BOXGPSHOLD|headFreeMode<<BOXHEADFREE|
+                 passThruMode<<BOXPASSTHRU|rcOptions[BOXBEEPERON]<<BOXBEEPERON);
+     tailSerialReply();break;
+   case MSP_RAW_IMU:
+     headSerialReply(c,18);
+     for(uint8_t i=0;i<3;i++) serialize16(accSmooth[i]);
+     for(uint8_t i=0;i<3;i++) serialize16(gyroData[i]);
+     for(uint8_t i=0;i<3;i++) serialize16(magADC[i]);
+     tailSerialReply();break;
+   case MSP_SERVO:
+     headSerialReply(c,16);
+     for(uint8_t i=0;i<8;i++) serialize16(servo[i]);
+     tailSerialReply();break;
+   case MSP_MOTOR:
+     headSerialReply(c,16);
+     for(uint8_t i=0;i<8;i++) serialize16(motor[i]);
+     tailSerialReply();break;
+   case MSP_RC:
+     headSerialReply(c,16);
+     for(uint8_t i=0;i<8;i++) serialize16(rcData[i]);
+     tailSerialReply();break;
+   case MSP_RAW_GPS:
+     headSerialReply(c,14);
+     serialize8(GPS_fix);
+     serialize8(GPS_numSat);
+     serialize32(GPS_coord[LAT]);
+     serialize32(GPS_coord[LON]);
+     serialize16(GPS_altitude);
+     serialize16(GPS_speed);
+     tailSerialReply();break;
+   case MSP_COMP_GPS:
+     headSerialReply(c,5);
+     serialize16(GPS_distanceToHome);
+     serialize16(GPS_directionToHome);
+     serialize8(GPS_update);
+     tailSerialReply();break;
+   case MSP_ATTITUDE:
+     headSerialReply(c,6);
+     for(uint8_t i=0;i<2;i++) serialize16(angle[i]);
+     serialize16(heading);
+     tailSerialReply();break;
+   case MSP_ALTITUDE:
+     headSerialReply(c,4);
+     serialize32(EstAlt);
+     tailSerialReply();break;
+   case MSP_BAT:
+     headSerialReply(c,3);
+     serialize8(vbat);
+     serialize16(intPowerMeterSum);
+     tailSerialReply();break;
+   case MSP_RC_TUNING:
+     headSerialReply(c,7);
+     serialize8(conf.rcRate8);
+     serialize8(conf.rcExpo8);
+     serialize8(conf.rollPitchRate);
+     serialize8(conf.yawRate);
+     serialize8(conf.dynThrPID);
+     serialize8(conf.thrMid8);
+     serialize8(conf.thrExpo8);
+     tailSerialReply();break;
+   case MSP_PID:
+     headSerialReply(c,3*PIDITEMS);
+     for(uint8_t i=0;i<PIDITEMS;i++)    {serialize8(conf.P8[i]);serialize8(conf.I8[i]);serialize8(conf.D8[i]);}
+     tailSerialReply();break;
+   case MSP_BOX:
+     headSerialReply(c,2*CHECKBOXITEMS);
+     for(uint8_t i=0;i<CHECKBOXITEMS;i++)    {serialize16(conf.activate[i]);}
+     tailSerialReply();break;
+   case MSP_MISC:
+     headSerialReply(c,2);
+     serialize16(intPowerTrigger1);
+     tailSerialReply();break;
+   case MSP_MOTOR_PINS:
+     headSerialReply(c,8);
+     for(uint8_t i=0;i<8;i++) {serialize8(PWM_PIN[i]);}
+     tailSerialReply();break;
+   case MSP_RESET_CONF:
+     conf.checkNewConf++;checkFirstTime();break;
+   case MSP_ACC_CALIBRATION:
+     calibratingA=400;break;
+   case MSP_MAG_CALIBRATION:
+     calibratingM=1;break;
+   case MSP_EEPROM_WRITE:
+     writeParams(0);break;
+   case MSP_DEBUG:
+     headSerialReply(c,8);
+     serialize16(debug1); // 4 variables are here for general monitoring purpose
+     serialize16(debug2);
+     serialize16(debug3);
+     serialize16(debug4);
+     tailSerialReply();break;
   }
 }
 
