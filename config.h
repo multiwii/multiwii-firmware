5a4b02cc8a68d2a33b4af64ec4ebba4f75357dda
nils.tholen@gmail.com <nils.tholen@gmail.com@02679b44-d973-9852-f2fa-63770883b36c>
2012-07-07 19:11:49 +0000

    included armedtimewarning
    git-svn-id: http://multiwii.googlecode.com/svn/trunk/MultiWii_shared@969 02679b44-d973-9852-f2fa-63770883b36c

--- config.h
+++ config.h
@@ -701,6 +701,7 @@
   /********************************************************************/
     //#define BUZZER
     //#define RCOPTIONSBEEP        //uncomment this if you want the buzzer to beep at any rcOptions change on channel Aux1 to Aux4
+	//#define ARMEDTIMEWARNING 330  // Trigger an alarm after a certain time of being armed [s] to save you lipo (if your TX does not have a countdown)
 
   /********************************************************************/
   /****           battery voltage monitoring                       ****/
