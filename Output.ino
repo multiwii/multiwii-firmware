fdd2a97c1bb94626463eb9d7e91de668ca16a20d
dubusal@gmail.com <dubusal@gmail.com@02679b44-d973-9852-f2fa-63770883b36c>
2012-09-23 17:10:13 +0000

    - bug correction about software PWM for a 6 motor config on 328p
    git-svn-id: http://multiwii.googlecode.com/svn/trunk/MultiWii_shared@1133 02679b44-d973-9852-f2fa-63770883b36c

--- Output.ino
+++ Output.ino
@@ -722,9 +722,15 @@ void initializeServo() {
         SOFT_PWM_CHANNEL1 += atomicPWM_PIN5_highState;
         state = 1;
       }else if(state == 1){
+        SOFT_PWM_CHANNEL1 += atomicPWM_PIN5_highState;
+        state = 2;
+      }else if(state == 2){
         SOFT_PWM_1_PIN_LOW;
         SOFT_PWM_CHANNEL1 += atomicPWM_PIN5_lowState;
-        state = 0;  
+        state = 3;  
+      }else if(state == 3){
+        SOFT_PWM_CHANNEL1 += atomicPWM_PIN5_lowState;
+        state = 0;   
       }
     }
     ISR(SOFT_PWM_ISR2) { 
@@ -734,9 +740,15 @@ void initializeServo() {
         SOFT_PWM_CHANNEL2 += atomicPWM_PIN6_highState;
         state = 1;
       }else if(state == 1){
+        SOFT_PWM_CHANNEL2 += atomicPWM_PIN6_highState;
+        state = 2;
+      }else if(state == 2){
         SOFT_PWM_2_PIN_LOW;
         SOFT_PWM_CHANNEL2 += atomicPWM_PIN6_lowState;
-        state = 0;  
+        state = 3;  
+      }else if(state == 3){
+        SOFT_PWM_CHANNEL2 += atomicPWM_PIN6_lowState;
+        state = 0;   
       }
     }
   #else
