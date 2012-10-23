14e89f2261e5382bae45bf7a3fcf99e0e72c4bca
gary_moscardini@mitel.com <gary_moscardini@mitel.com@02679b44-d973-9852-f2fa-63770883b36c>
2012-10-23 08:59:24 +0000

    SERVO_MIX_TILT gimbal to support definable AUX channels
    git-svn-id: http://multiwii.googlecode.com/svn/trunk/MultiWii_shared@1227 02679b44-d973-9852-f2fa-63770883b36c

--- Output.ino
+++ Output.ino
@@ -958,11 +958,11 @@ void mixTable() {
     } else {
         // to use it with A0_A1_PIN_HEX
       #if defined(A0_A1_PIN_HEX) && (NUMBER_MOTOR == 6) && defined(PROMINI)
-        servo[2] = constrain(TILT_PITCH_MIDDLE  + rcData[AUX3]-1500 , TILT_PITCH_MIN, TILT_PITCH_MAX);
-        servo[3] = constrain(TILT_ROLL_MIDDLE   + rcData[AUX4]-1500,  TILT_ROLL_MIN, TILT_ROLL_MAX);     
+        servo[2] = constrain(TILT_PITCH_MIDDLE  + rcData[TILT_PITCH_AUX_CH]-1500 , TILT_PITCH_MIN, TILT_PITCH_MAX);
+        servo[3] = constrain(TILT_ROLL_MIDDLE   + rcData[TILT_ROLL_AUX_CH]-1500,  TILT_ROLL_MIN, TILT_ROLL_MAX);     
       #else
-        servo[0] = constrain(TILT_PITCH_MIDDLE  + rcData[AUX3]-1500 , TILT_PITCH_MIN, TILT_PITCH_MAX);
-        servo[1] = constrain(TILT_ROLL_MIDDLE   + rcData[AUX4]-1500,  TILT_ROLL_MIN, TILT_ROLL_MAX);
+        servo[0] = constrain(TILT_PITCH_MIDDLE  + rcData[TILT_PITCH_AUX_CH]-1500 , TILT_PITCH_MIN, TILT_PITCH_MAX);
+        servo[1] = constrain(TILT_ROLL_MIDDLE   + rcData[TILT_ROLL_AUX_CH]-1500,  TILT_ROLL_MIN, TILT_ROLL_MAX);
       #endif
     }
   #endif  
