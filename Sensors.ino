ea664fef959a4fb29873916a87b426475de75c97
eosbandi@gmail.com <eosbandi@gmail.com@02679b44-d973-9852-f2fa-63770883b36c>
2012-12-13 22:31:52 +0000

    Reworked mag gain calibration for HMC5883L, based on Fabio's FreeIMU code. Now it is as good as it could be on this processor... (No changes in the mag calibration dance, gain calibration is done automatically at every startup). To compensate hard iron offsets yo have to do the dance...
    git-svn-id: http://multiwii.googlecode.com/svn/trunk/MultiWii_shared@1284 02679b44-d973-9852-f2fa-63770883b36c

--- Sensors.ino
+++ Sensors.ino
@@ -1018,7 +1018,7 @@ void Gyro_getADC () {
 // I2C Compass common function
 // ************************************************************************************************************
 #if MAG
-static float   magCal[3] = {1.0,1.0,1.0};  // gain for each axis, populated at sensor init
+static float   magGain[3] = {1.0,1.0,1.0};  // gain for each axis, populated at sensor init
 static uint8_t magInit = 0;
 
 uint8_t Mag_getADC() { // return 1 when news values are available, 0 otherwise
@@ -1030,9 +1030,9 @@ uint8_t Mag_getADC() { // return 1 when news values are available, 0 otherwise
   t = currentTime + 100000;
   TWBR = ((F_CPU / 400000L) - 16) / 2; // change the I2C clock rate to 400kHz
   Device_Mag_getADC();
-  magADC[ROLL]  = magADC[ROLL]  * magCal[ROLL];
-  magADC[PITCH] = magADC[PITCH] * magCal[PITCH];
-  magADC[YAW]   = magADC[YAW]   * magCal[YAW];
+  magADC[ROLL]  = magADC[ROLL]  * magGain[ROLL];
+  magADC[PITCH] = magADC[PITCH] * magGain[PITCH];
+  magADC[YAW]   = magADC[YAW]   * magGain[YAW];
   if (f.CALIBRATE_MAG) {
     tCal = t;
     for(axis=0;axis<3;axis++) {
@@ -1106,13 +1106,116 @@ uint8_t Mag_getADC() { // return 1 when news values are available, 0 otherwise
     }
   #endif
 #endif
-
 // ************************************************************************************************************
-// I2C Compass HMC5843 & HMC5883
+// I2C Compass HMC5883
 // ************************************************************************************************************
 // I2C adress: 0x3C (8bit)   0x1E (7bit)
 // ************************************************************************************************************
-#if defined(HMC5843) || defined(HMC5883)
+
+#if defined(HMC5883)
+
+#define HMC58X3_R_CONFA 0
+#define HMC58X3_R_CONFB 1
+#define HMC58X3_R_MODE 2
+#define HMC58X3_X_SELF_TEST_GAUSS (+1.16)                       //!< X axis level when bias current is applied.
+#define HMC58X3_Y_SELF_TEST_GAUSS (+1.16)   //!< Y axis level when bias current is applied.
+#define HMC58X3_Z_SELF_TEST_GAUSS (+1.08)                       //!< Y axis level when bias current is applied.
+#define SELF_TEST_LOW_LIMIT  (243.0/390.0)   //!< Low limit when gain is 5.
+#define SELF_TEST_HIGH_LIMIT (575.0/390.0)   //!< High limit when gain is 5.
+#define HMC_POS_BIAS 1
+#define HMC_NEG_BIAS 2
+
+#define MAG_ADDRESS 0x1E
+#define MAG_DATA_REGISTER 0x03
+
+void Mag_init() {
+
+    int32_t xyz_total[3]={0,0,0};  // 32 bit totals so they won't overflow.
+    bool bret=true;                // Error indicator
+	
+	delay(50);						//Wait before start
+	i2c_writeReg(MAG_ADDRESS, HMC58X3_R_CONFA, 0x010 + HMC_POS_BIAS); // Reg A DOR=0x010 + MS1,MS0 set to pos bias
+
+	// Note that the  very first measurement after a gain change maintains the same gain as the previous setting. 
+    // The new gain setting is effective from the second measurement and on.
+
+	i2c_writeReg(MAG_ADDRESS, HMC58X3_R_CONFB, 2 << 5);  //Set the Gain
+	i2c_writeReg(MAG_ADDRESS,HMC58X3_R_MODE, 1);
+    delay(100);
+	getADC();									//Get one sample, and discard it
+
+            for (uint8_t i=0; i<10; i++)		//Collect 10 samples
+            { 
+
+				i2c_writeReg(MAG_ADDRESS,HMC58X3_R_MODE, 1);
+				delay(100);
+                getADC();   // Get the raw values in case the scales have already been changed.
+                
+				// Since the measurements are noisy, they should be averaged rather than taking the max.
+                xyz_total[0]+=magADC[0];
+                xyz_total[1]+=magADC[1];
+                xyz_total[2]+=magADC[2];
+                
+				// Detect saturation.
+                if (-(1<<12) >= min(magADC[0],min(magADC[1],magADC[2])))
+                {
+                    bret=false;
+                    break;  // Breaks out of the for loop.  No sense in continuing if we saturated.
+                }
+            }
+
+			// Apply the negative bias. (Same gain)
+            i2c_writeReg(MAG_ADDRESS,HMC58X3_R_CONFA, 0x010 + HMC_NEG_BIAS); // Reg A DOR=0x010 + MS1,MS0 set to negative bias.
+            for (unsigned int i=0; i<10; i++) 
+            { 
+				i2c_writeReg(MAG_ADDRESS,HMC58X3_R_MODE, 1);
+				delay(100);
+                getADC();   // Get the raw values in case the scales have already been changed.
+                
+				// Since the measurements are noisy, they should be averaged.
+                xyz_total[0]-=magADC[0];
+                xyz_total[1]-=magADC[1];
+                xyz_total[2]-=magADC[2];
+
+				// Detect saturation.
+                if (-(1<<12) >= min(magADC[0],min(magADC[1],magADC[2])))
+                {
+                    bret=false;
+                    break;  // Breaks out of the for loop.  No sense in continuing if we saturated.
+                }
+            }
+
+			magGain[0]=fabs((820.0*(HMC58X3_X_SELF_TEST_GAUSS*2.0))/(xyz_total[0]/10));
+            magGain[1]=fabs((820.0*(HMC58X3_Y_SELF_TEST_GAUSS*2.0))/(xyz_total[1]/10));
+            magGain[2]=fabs((820.0*(HMC58X3_Z_SELF_TEST_GAUSS*2.0))/(xyz_total[2]/10));
+
+			// leave test mode
+			i2c_writeReg(MAG_ADDRESS ,HMC58X3_R_CONFA ,0x70 ); //Configuration Register A  -- 0 11 100 00  num samples: 8 ; output rate: 15Hz ; normal measurement mode
+			i2c_writeReg(MAG_ADDRESS ,HMC58X3_R_CONFB ,0x20 ); //Configuration Register B  -- 001 00000    configuration gain 1.3Ga
+			i2c_writeReg(MAG_ADDRESS ,HMC58X3_R_MODE ,0x00 ); //Mode register             -- 000000 00    continuous Conversion Mode
+			delay(100);
+			magInit = 1;
+
+			if (!bret)			//Something went wrong so get a best guess
+			{
+				magGain[0] = 1.0;
+				magGain[1] = 1.0;
+				magGain[2] = 1.0;
+			}
+
+
+ }
+ //  Mag_init().
+
+#endif
+
+// ************************************************************************************************************
+// I2C Compass HMC5843
+// ************************************************************************************************************
+// I2C adress: 0x3C (8bit)   0x1E (7bit)
+// ************************************************************************************************************
+#if defined(HMC5843)
+
   #define MAG_ADDRESS 0x1E
   #define MAG_DATA_REGISTER 0x03
   
@@ -1131,15 +1234,9 @@ uint8_t Mag_getADC() { // return 1 when news values are available, 0 otherwise
     delay(100);
       getADC();
     delay(10);
-    #if defined(HMC5883)
-      magCal[ROLL]  =  1160.0 / abs(magADC[ROLL]);
-      magCal[PITCH] =  1160.0 / abs(magADC[PITCH]);
-      magCal[YAW]   =  1080.0 / abs(magADC[YAW]);
-    #else
-      magCal[ROLL]  =  1000.0 / abs(magADC[ROLL]);
-      magCal[PITCH] =  1000.0 / abs(magADC[PITCH]);
-      magCal[YAW]   =  1000.0 / abs(magADC[YAW]);
-    #endif
+      magGain[ROLL]  =  1000.0 / abs(magADC[ROLL]);
+      magGain[PITCH] =  1000.0 / abs(magADC[PITCH]);
+      magGain[YAW]   =  1000.0 / abs(magADC[YAW]);
 
     // leave test mode
     i2c_writeReg(MAG_ADDRESS ,0x00 ,0x70 ); //Configuration Register A  -- 0 11 100 00  num samples: 8 ; output rate: 15Hz ; normal measurement mode
@@ -1148,7 +1245,9 @@ uint8_t Mag_getADC() { // return 1 when news values are available, 0 otherwise
 
     magInit = 1;
   }
-
+#endif
+  
+#if defined(HMC5843) || defined(HMC5883)
 void getADC() {
   i2c_getSixRawADC(MAG_ADDRESS,MAG_DATA_REGISTER);
   #if defined(HMC5843)
@@ -1168,6 +1267,7 @@ void Device_Mag_getADC() {
   getADC();
 }
 #endif
+
 #endif
 
 // ************************************************************************************************************
