45c62e095a48565ec94a1aa05a17173cbcc2282e
dubusal@gmail.com <dubusal@gmail.com@02679b44-d973-9852-f2fa-63770883b36c>
2013-05-14 21:02:48 +0000

    - correction of L3G4200D gyro settings
    git-svn-id: http://multiwii.googlecode.com/svn/trunk/MultiWii_shared@1439 02679b44-d973-9852-f2fa-63770883b36c

--- Sensors.ino
+++ Sensors.ino
@@ -986,15 +986,17 @@ void Gyro_init() {
   i2c_writeReg(L3G4200D_ADDRESS ,0x20 ,0x8F ); // CTRL_REG1   400Hz ODR, 20hz filter, run!
   delay(5);
   i2c_writeReg(L3G4200D_ADDRESS ,0x24 ,0x02 ); // CTRL_REG5   low pass filter enable
+  delay(5);
+  i2c_writeReg(L3G4200D_ADDRESS ,0x23 ,0x30); // CTRL_REG4 Select 2000dps
 }
 
 void Gyro_getADC () {
   TWBR = ((F_CPU / 400000L) - 16) / 2; // change the I2C clock rate to 400kHz
   i2c_getSixRawADC(L3G4200D_ADDRESS,0x80|0x28);
 
-  GYRO_ORIENTATION( ((rawADC[1]<<8) | rawADC[0])/20  ,
-                    ((rawADC[3]<<8) | rawADC[2])/20  ,
-                    ((rawADC[5]<<8) | rawADC[4])/20  );
+  GYRO_ORIENTATION( ((rawADC[1]<<8) | rawADC[0])>>2  ,
+                    ((rawADC[3]<<8) | rawADC[2])>>2  ,
+                    ((rawADC[5]<<8) | rawADC[4])>>2  );
   GYRO_Common();
 }
 #endif
