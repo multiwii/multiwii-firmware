64085db846816bec488626d0783bfae6b0b4e2c0
virtume2@gmail.com <virtume2@gmail.com@02679b44-d973-9852-f2fa-63770883b36c>
2013-05-25 20:22:13 +0000

    Update to MultiWii_32U4_SE
    git-svn-id: http://multiwii.googlecode.com/svn/trunk/MultiWii_shared@1451 02679b44-d973-9852-f2fa-63770883b36c

--- def.h
+++ def.h
@@ -1417,17 +1417,18 @@
 #if defined(MultiWii_32U4_SE)
   #define MPU6050
   #define HMC5883
+  #define MPU6050_I2C_AUX_MASTER // MAG connected to the AUX I2C bus of MPU6050
   #define MS561101BA
   #define ACC_ORIENTATION(X, Y, Z)  {accADC[ROLL]  = -X; accADC[PITCH]  = -Y; accADC[YAW]  =  Z;}
   #define GYRO_ORIENTATION(X, Y, Z) {gyroADC[ROLL] =  Y; gyroADC[PITCH] = -X; gyroADC[YAW] = -Z;}
   #define MAG_ORIENTATION(X, Y, Z)  {magADC[ROLL]  =  X; magADC[PITCH]  =  Y; magADC[YAW]  = -Z;}
-  #define MPU6050_I2C_AUX_MASTER // MAG connected to the AUX I2C bus of MPU6050
   #undef INTERNAL_I2C_PULLUPS
 #endif
 
 #if defined(MultiWii_32U4_SE_no_baro)
   #define MPU6050
   #define HMC5883
+  #define MPU6050_I2C_AUX_MASTER // MAG connected to the AUX I2C bus of MPU6050
   #define ACC_ORIENTATION(X, Y, Z)  {accADC[ROLL]  = -X; accADC[PITCH]  = -Y; accADC[YAW]  =  Z;}
   #define GYRO_ORIENTATION(X, Y, Z) {gyroADC[ROLL] =  Y; gyroADC[PITCH] = -X; gyroADC[YAW] = -Z;}
   #define MAG_ORIENTATION(X, Y, Z)  {magADC[ROLL]  =  X; magADC[PITCH]  =  Y; magADC[YAW]  = -Z;}
