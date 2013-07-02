b5444b1b3d4c48e6deb83653e4699f957ecf4cc8
virtume2@gmail.com <virtume2@gmail.com@02679b44-d973-9852-f2fa-63770883b36c>
2013-07-02 19:16:48 +0000

    Comment for FLYDUINO_MPU
    git-svn-id: http://multiwii.googlecode.com/svn/trunk/MultiWii_shared@1506 02679b44-d973-9852-f2fa-63770883b36c

--- config.h
+++ config.h
@@ -134,7 +134,7 @@
       //#define Bobs_6DOF_V1     // BobsQuads 6DOF V1 with ITG3200 & BMA180
       //#define Bobs_9DOF_V1     // BobsQuads 9DOF V1 with ITG3200, BMA180 & HMC5883L
       //#define Bobs_10DOF_BMP_V1 // BobsQuads 10DOF V1 with ITG3200, BMA180, HMC5883L & BMP180 - BMP180 is software compatible with BMP085
-      //#define FLYDUINO_MPU
+      //#define FLYDUINO_MPU       // MPU6050 Break Out onboard 3.3V reg
       //#define CRIUS_AIO_PRO_V1
       //#define DESQUARED6DOFV2GO  // DEsquared V2 with ITG3200 only
       //#define DESQUARED6DOFV4    // DEsquared V4 with MPU6050
