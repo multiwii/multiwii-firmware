283da490e90b5387c65171c1a861b706fd53e831
dubusal@gmail.com <dubusal@gmail.com@02679b44-d973-9852-f2fa-63770883b36c>
2013-11-11 23:20:23 +0000

    - small I2C function shortcut
    git-svn-id: http://multiwii.googlecode.com/svn/trunk/MultiWii_shared@1620 02679b44-d973-9852-f2fa-63770883b36c

--- Sensors.cpp
+++ Sensors.cpp
@@ -243,23 +243,15 @@ void waitTransmissionI2C() {
   }
 }
 
-size_t i2c_read_to_buf(uint8_t add, void *buf, size_t size) {
+void i2c_read_reg_to_buf(uint8_t add, uint8_t reg, uint8_t *buf, uint8_t size) {
+  i2c_rep_start(add<<1); // I2C write direction
+  i2c_write(reg);        // register selection
   i2c_rep_start((add<<1) | 1);  // I2C read direction
-  size_t bytes_read = 0;
-  uint8_t *b = (uint8_t*)buf;
+  uint8_t *b = buf;
   while (size--) {
     /* acknowledge all but the final byte */
     *b++ = i2c_read(size > 0);
-    /* TODO catch I2C errors here and abort */
-    bytes_read++;
   }
-  return bytes_read;
-}
-
-size_t i2c_read_reg_to_buf(uint8_t add, uint8_t reg, void *buf, size_t size) {
-  i2c_rep_start(add<<1); // I2C write direction
-  i2c_write(reg);        // register selection
-  return i2c_read_to_buf(add, buf, size);
 }
 
 /* transform a series of bytes from big endian to little
@@ -280,7 +272,7 @@ void swap_endianness(void *buf, size_t size) {
 }
 
 void i2c_getSixRawADC(uint8_t add, uint8_t reg) {
-  i2c_read_reg_to_buf(add, reg, &rawADC, 6);
+  i2c_read_reg_to_buf(add, reg, rawADC, 6);
 }
 
 void i2c_writeReg(uint8_t add, uint8_t reg, uint8_t val) {
