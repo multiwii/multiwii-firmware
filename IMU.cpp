0ca21730f63b0a6c399e106860550f690f1b52a2
dubusal@gmail.com <dubusal@gmail.com@02679b44-d973-9852-f2fa-63770883b36c>
2013-11-29 18:19:47 +0000

    - use only one struct 3x32 bits for each  gyro+acc / gyro+mag  vector
    git-svn-id: http://multiwii.googlecode.com/svn/trunk/MultiWii_shared@1637 02679b44-d973-9852-f2fa-63770883b36c

--- IMU.cpp
+++ IMU.cpp
@@ -104,7 +104,7 @@ void computeIMU () {
 /* Set the Gyro Weight for Gyro/Acc complementary filter
    Increasing this value would reduce and delay Acc influence on the output of the filter*/
 #ifndef GYR_CMPF_FACTOR
-  #define GYR_CMPF_FACTOR 9 //  that means a CMP_FACTOR of 512 (2^9)
+  #define GYR_CMPF_FACTOR 10 //  that means a CMP_FACTOR of 1024 (2^10)
 #endif
 
 /* Set the Gyro Weight for Gyro/Magnetometer complementary filter
@@ -112,23 +112,23 @@ void computeIMU () {
 #define GYR_CMPFM_FACTOR 8 // that means a CMP_FACTOR of 256 (2^8)
 
 
-typedef struct int32_t_vector {
+typedef struct  {
   int32_t X,Y,Z;
 } t_int32_t_vector_def;
 
-typedef union {
-  int32_t A[3];
-  t_int32_t_vector_def V;
-} t_int32_t_vector;
-
-typedef struct int16_t_vector {
-  int16_t X,Y,Z;
+typedef struct  {
+  uint16_t XL; int16_t X;
+  uint16_t YL; int16_t Y;
+  uint16_t ZL; int16_t Z;
 } t_int16_t_vector_def;
 
+// note: we use implicit first 16 MSB bits 32 -> 16 cast. ie V32.X>>16 = V16.X
 typedef union {
-  int16_t A[3];
-  t_int16_t_vector_def V;
-} t_int16_t_vector;
+  int32_t A32[3];
+  t_int32_t_vector_def V32;
+  int16_t A16[6];
+  t_int16_t_vector_def V16;
+} t_int32_t_vector;
 
 //return angle , unit: 1/10 degree
 int16_t _atan2(int32_t y, int32_t x){
@@ -198,14 +198,14 @@ int32_t  __attribute__ ((noinline)) mul(int16_t a, int16_t b) {
 }
 
 // Rotate Estimated vector(s) with small angle approximation, according to the gyro data
-void rotateV32(struct int32_t_vector *v,int16_t* delta) {
-  int16_t X = v->X>>16;
-  int16_t Y = v->Y>>16;
-  int16_t Z = v->Z>>16;
-  
-  v->Z -=  mul(delta[ROLL]  ,  X)  + mul(delta[PITCH] , Y);
-  v->X +=  mul(delta[ROLL]  ,  Z)  - mul(delta[YAW]   , Y);
-  v->Y +=  mul(delta[PITCH] ,  Z)  + mul(delta[YAW]   , X);
+void rotateV32( t_int32_t_vector *v,int16_t* delta) {
+  int16_t X = v->V16.X;
+  int16_t Y = v->V16.Y;
+  int16_t Z = v->V16.Z;
+
+  v->V32.Z -=  mul(delta[ROLL]  ,  X)  + mul(delta[PITCH] , Y);
+  v->V32.X +=  mul(delta[ROLL]  ,  Z)  - mul(delta[YAW]   , Y);
+  v->V32.Y +=  mul(delta[PITCH] ,  Z)  + mul(delta[YAW]   , X);
 }
 
 static int16_t accZ=0;
@@ -215,8 +215,8 @@ void getEstimatedAttitude(){
   int32_t accMag = 0;
   float scale;
   int16_t deltaGyroAngle16[3];
-  static t_int32_t_vector LPFA,LPFM,LPFAcc;
-  t_int16_t_vector EstG16,EstM16;
+  static t_int32_t_vector EstG,EstM;
+  static uint32_t LPFAcc[3];
   float invG; // 1/|G|
   static int16_t accZoffset = 0;
   int32_t accZ_tmp=0;
@@ -230,60 +230,58 @@ void getEstimatedAttitude(){
 
   // Initialization
   for (axis = 0; axis < 3; axis++) {
+    // valid as long as LPF_FACTOR is less than 15
+    imu.accSmooth[axis]  = LPFAcc[axis]>>ACC_LPF_FACTOR;
+    LPFAcc[axis]      += imu.accADC[axis] - imu.accSmooth[axis];
+    // used to calculate later the magnitude of acc vector
+    accMag   += mul(imu.accSmooth[axis] , imu.accSmooth[axis]);
     // unit: radian scaled by 2^16
     // imu.gyroADC[axis] is 14 bit long, the scale factor ensure deltaGyroAngle16[axis] is still 14 bit long
     deltaGyroAngle16[axis] = imu.gyroADC[axis]  * scale;
-    // valid as long as LPF_FACTOR is less than 15
-    imu.accSmooth[axis]  = LPFAcc.A[axis]>>ACC_LPF_FACTOR;
-    LPFAcc.A[axis]      += imu.accADC[axis] - imu.accSmooth[axis];
-    // used to calculate later the magnitude of acc vector
-    accMag   += mul(imu.accSmooth[axis] , imu.accSmooth[axis]);
   }
-  
+
   // we rotate the intermediate 32 bit vector with the radian vector (deltaGyroAngle16), scaled by 2^16
   // however, only the first 16 MSB of the 32 bit vector is used to compute the result
   // it is ok to use this approximation as the 16 LSB are used only for the complementary filter part
-  rotateV32(&LPFA.V,deltaGyroAngle16);
+  rotateV32(&EstG,deltaGyroAngle16);
   #if MAG
-    rotateV32(&LPFM.V,deltaGyroAngle16);
+    rotateV32(&EstM,deltaGyroAngle16);
   #endif
 
   // Apply complimentary filter (Gyro drift correction)
   // If accel magnitude >1.15G or <0.85G and ACC vector outside of the limit range => we neutralize the effect of accelerometers in the angle estimation.
   // To do that, we just skip filter, as EstV already rotated by Gyro
   for (axis = 0; axis < 3; axis++) {
-    EstG16.A[axis] = LPFA.A[axis]>>16;
     if ( (int16_t)(0.85*ACC_1G*ACC_1G/256) < (int16_t)(accMag>>8) && (int16_t)(accMag>>8) < (int16_t)(1.15*ACC_1G*ACC_1G/256) )
-      LPFA.A[axis] += (int32_t)(imu.accSmooth[axis] - EstG16.A[axis])<<(16-GYR_CMPF_FACTOR);
-    accZ_tmp += mul(imu.accSmooth[axis] , EstG16.A[axis]);
+      EstG.A32[axis] += (int32_t)(imu.accSmooth[axis] - EstG.A16[2*axis+1])<<(16-GYR_CMPF_FACTOR);
+    accZ_tmp += mul(imu.accSmooth[axis] , EstG.A16[2*axis+1]);
     #if MAG
-      EstM16.A[axis] = LPFM.A[axis]>>16;
-      LPFM.A[axis]  += (int32_t)(imu.magADC[axis] - EstM16.A[axis])<<(16-GYR_CMPFM_FACTOR);
+      EstM.A32[axis]  += (int32_t)(imu.magADC[axis] - EstM.A16[2*axis+1])<<(16-GYR_CMPFM_FACTOR);
     #endif
   }
   
-  if (EstG16.A[2] > ACCZ_25deg)
+  if (EstG.V16.Z > ACCZ_25deg)
     f.SMALL_ANGLES_25 = 1;
   else
     f.SMALL_ANGLES_25 = 0;
 
   // Attitude of the estimated vector
-  int32_t sqGX_sqGZ = mul(EstG16.V.X,EstG16.V.X) + mul(EstG16.V.Z,EstG16.V.Z);
-  invG = InvSqrt(sqGX_sqGZ + mul(EstG16.V.Y,EstG16.V.Y));
-  att.angle[ROLL]  = _atan2(EstG16.V.X , EstG16.V.Z);
-  att.angle[PITCH] = _atan2(EstG16.V.Y , InvSqrt(sqGX_sqGZ)*sqGX_sqGZ);
+  int32_t sqGX_sqGZ = mul(EstG.V16.X,EstG.V16.X) + mul(EstG.V16.Z,EstG.V16.Z);
+  invG = InvSqrt(sqGX_sqGZ + mul(EstG.V16.Y,EstG.V16.Y));
+  att.angle[ROLL]  = _atan2(EstG.V16.X , EstG.V16.Z);
+  att.angle[PITCH] = _atan2(EstG.V16.Y , InvSqrt(sqGX_sqGZ)*sqGX_sqGZ);
 
   #if MAG
     //note on the second term: mathematically there is a risk of overflow (16*16*16=48 bits). assumed to be null with real values
     att.heading = _atan2(
-      mul(EstM16.V.Z , EstG16.V.X) - mul(EstM16.V.X , EstG16.V.Z),
-      (EstM16.V.Y * sqGX_sqGZ  - (mul(EstM16.V.X , EstG16.V.X) + mul(EstM16.V.Z , EstG16.V.Z)) * EstG16.V.Y)*invG );
+      mul(EstM.V16.Z , EstG.V16.X) - mul(EstM.V16.X , EstG.V16.Z),
+      (EstM.V16.Y * sqGX_sqGZ  - (mul(EstM.V16.X , EstG.V16.X) + mul(EstM.V16.Z , EstG.V16.Z)) * EstG.V16.Y)*invG );
     att.heading += conf.mag_declination; // Set from GUI
     att.heading /= 10;
   #endif
 
   #if defined(THROTTLE_ANGLE_CORRECTION)
-    cosZ = EstG16.V.Z / (ACC_1G * 100.0f);                                                        // cos(angleZ) * 100 
+    cosZ = EstG.V16.Z / (ACC_1G * 100.0f);                                                        // cos(angleZ) * 100 
     throttleAngleCorrection = THROTTLE_ANGLE_CORRECTION * constrain(100 - cosZ, 0, 100) >>3;  // 16 bit ok: 200*150 = 30000  
   #endif
 
@@ -373,4 +371,3 @@ uint8_t getEstimatedAltitude(){
   return 1;
 }
 #endif //BARO
-
