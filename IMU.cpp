35a9cb2808c835d3980d60f3e66faa9421365679
dubusal@gmail.com <dubusal@gmail.com@02679b44-d973-9852-f2fa-63770883b36c>
2013-12-24 13:31:21 +0000

    - THROTTLE_ANGLE_CORRECTION bug correction (http://www.multiwii.com/forum/viewtopic.php?f=8&t=4433)
    git-svn-id: http://multiwii.googlecode.com/svn/trunk/MultiWii_shared@1640 02679b44-d973-9852-f2fa-63770883b36c

--- IMU.cpp
+++ IMU.cpp
@@ -281,7 +281,7 @@ void getEstimatedAttitude(){
   #endif
 
   #if defined(THROTTLE_ANGLE_CORRECTION)
-    cosZ = EstG.V16.Z / (ACC_1G * 100.0f);                                                        // cos(angleZ) * 100 
+    cosZ = mul(EstG.V16.Z , 100) / ACC_1G ;                                                   // cos(angleZ) * 100 
     throttleAngleCorrection = THROTTLE_ANGLE_CORRECTION * constrain(100 - cosZ, 0, 100) >>3;  // 16 bit ok: 200*150 = 30000  
   #endif
 
