5b1b0977ccd4433b1741c1f7c241237dabd73a83
dubusal <dubusal@02679b44-d973-9852-f2fa-63770883b36c>
2014-10-11 21:35:58 +0000

    - BARO MS code tweak
    git-svn-id: http://multiwii.googlecode.com/svn/trunk/MultiWii_shared@1713 02679b44-d973-9852-f2fa-63770883b36c

--- Sensors.cpp
+++ Sensors.cpp
@@ -434,7 +434,7 @@ static void Baro_Common() {
   baroHistTab[baroHistIdx] = baroPressure;
   baroPressureSum += baroHistTab[baroHistIdx];
   baroPressureSum -= baroHistTab[indexplus1];
-  baroHistIdx = indexplus1;  
+  baroHistIdx = indexplus1;
 }
 #endif
 
@@ -609,70 +609,45 @@ uint8_t Baro_update() {                   // first UT conversion is started in i
 static struct {
   // sensor registers from the MS561101BA datasheet
   uint16_t c[7];
-  union {uint32_t val; uint8_t raw[4]; } ut; //uncompensated T
-  union {uint32_t val; uint8_t raw[4]; } up; //uncompensated P
+  uint32_t ut; //uncompensated T
+  uint32_t up; //uncompensated P
   uint8_t  state;
   uint16_t deadline;
 } ms561101ba_ctx;
 
-static void i2c_MS561101BA_reset(){
+static void Baro_init() {
+  //reset
   i2c_writeReg(MS561101BA_ADDRESS, MS561101BA_RESET, 0);
-}
-
-static void i2c_MS561101BA_readCalibration(){
+  delay(100);
+  
+  //read calibration data
   union {uint16_t val; uint8_t raw[2]; } data;
   for(uint8_t i=0;i<6;i++) {
     i2c_rep_start(MS561101BA_ADDRESS<<1);
     i2c_write(0xA2+2*i);
-    delay(10);
     i2c_rep_start((MS561101BA_ADDRESS<<1) | 1);//I2C read direction => 1
-    delay(10);
     data.raw[1] = i2c_readAck();  // read a 16 bit register
     data.raw[0] = i2c_readNak();
     ms561101ba_ctx.c[i+1] = data.val;
   }
 }
 
-// read uncompensated temperature value: send command first
-static void i2c_MS561101BA_UT_Start() {
-  i2c_rep_start(MS561101BA_ADDRESS<<1);      // I2C write direction
-  i2c_write(MS561101BA_TEMPERATURE + OSR);  // register selection
+// read uncompensated temperature or pressure value: send command first
+static void i2c_MS561101BA_UT_or_UP_Start(uint8_t reg) {
+  i2c_rep_start(MS561101BA_ADDRESS<<1);     // I2C write direction
+  i2c_write(reg);  // register selection
   i2c_stop();
 }
 
-// read uncompensated pressure value: send command first
-static void i2c_MS561101BA_UP_Start () {
-  i2c_rep_start(MS561101BA_ADDRESS<<1);      // I2C write direction
-  i2c_write(MS561101BA_PRESSURE + OSR);     // register selection
-  i2c_stop();
-}
-
-// read uncompensated pressure value: read result bytes
-static void i2c_MS561101BA_UP_Read () {
+static void i2c_MS561101BA_UT_or_UP_Read(uint32_t* val) {
+  union {uint32_t val; uint8_t raw[4]; } data;
   i2c_rep_start(MS561101BA_ADDRESS<<1);
   i2c_write(0);
   i2c_rep_start((MS561101BA_ADDRESS<<1) | 1);
-  ms561101ba_ctx.up.raw[2] = i2c_readAck();
-  ms561101ba_ctx.up.raw[1] = i2c_readAck();
-  ms561101ba_ctx.up.raw[0] = i2c_readNak();
-}
-
-// read uncompensated temperature value: read result bytes
-static void i2c_MS561101BA_UT_Read() {
-  i2c_rep_start(MS561101BA_ADDRESS<<1);
-  i2c_write(0);
-  i2c_rep_start((MS561101BA_ADDRESS<<1) | 1);
-  ms561101ba_ctx.ut.raw[2] = i2c_readAck();
-  ms561101ba_ctx.ut.raw[1] = i2c_readAck();
-  ms561101ba_ctx.ut.raw[0] = i2c_readNak();
-}
-
-static void Baro_init() {
-  i2c_MS561101BA_reset();
-  delay(100);
-  i2c_MS561101BA_readCalibration();
-  i2c_MS561101BA_UT_Start(); 
-  ms561101ba_ctx.deadline = currentTime+10000; 
+  data.raw[2] = i2c_readAck();
+  data.raw[1] = i2c_readAck();
+  data.raw[0] = i2c_readNak();
+  *val = data.val;
 }
 
 // use float approximation instead of int64_t intermediate values
@@ -680,7 +655,7 @@ static void Baro_init() {
 static void i2c_MS561101BA_Calculate() {
   int32_t delt;
 
-  float dT        = (int32_t)ms561101ba_ctx.ut.val - (int32_t)((uint32_t)ms561101ba_ctx.c[5] << 8);
+  float dT        = (int32_t)ms561101ba_ctx.ut - (int32_t)((uint32_t)ms561101ba_ctx.c[5] << 8);
   float off       = ((uint32_t)ms561101ba_ctx.c[2] <<16) + ((dT * ms561101ba_ctx.c[4]) /((uint32_t)1<<7));
   float sens      = ((uint32_t)ms561101ba_ctx.c[1] <<15) + ((dT * ms561101ba_ctx.c[3]) /((uint32_t)1<<8));
   delt            = (dT * ms561101ba_ctx.c[6])/((uint32_t)1<<23);
@@ -690,30 +665,33 @@ static void i2c_MS561101BA_Calculate() {
     off  -= delt>>1; 
     sens -= delt>>2;
   }
-  baroPressure     = (( (ms561101ba_ctx.up.val * sens ) /((uint32_t)1<<21)) - off)/((uint32_t)1<<15);
+  baroPressure     = (( (ms561101ba_ctx.up * sens ) /((uint32_t)1<<21)) - off)/((uint32_t)1<<15);
 }
 
-//return 0: no data available, no computation ;  1: new value available  ; 2: no new value, but computation time
+//return 0: no data available, no computation ;  1: new value available   or   no new value but computation time
 uint8_t Baro_update() {                          // first UT conversion is started in init procedure
+  uint32_t* rawValPointer;
+  uint8_t   commandRegister;
+
   if (ms561101ba_ctx.state == 2) {               // a third state is introduced here to isolate calculate() and smooth timecycle spike
     ms561101ba_ctx.state = 0;
     i2c_MS561101BA_Calculate();
-    return 2;
+    return 1;
   }
-  if ((int16_t)(currentTime - ms561101ba_ctx.deadline)<0) return 0;
+  if ((int16_t)(currentTime - ms561101ba_ctx.deadline)<0) return 0; // the initial timer is not initialized, but in any case, no more than 65ms to wait.
   ms561101ba_ctx.deadline = currentTime+10000;  // UT and UP conversion take 8.5ms so we do next reading after 10ms 
   if (ms561101ba_ctx.state == 0) {
-    i2c_MS561101BA_UT_Read();
-    i2c_MS561101BA_UP_Start();
-    Baro_Common();                              // moved here for less timecycle spike
-    ms561101ba_ctx.state = 1;
-    return 1;
+    Baro_Common();                              // moved here for less timecycle spike, goes after i2c_MS561101BA_Calculate
+    rawValPointer = &ms561101ba_ctx.ut;
+    commandRegister = MS561101BA_PRESSURE + OSR;
   } else {
-    i2c_MS561101BA_UP_Read();
-    i2c_MS561101BA_UT_Start();
-    ms561101ba_ctx.state = 2;
-    return 2;
+    rawValPointer = &ms561101ba_ctx.up;
+    commandRegister = MS561101BA_TEMPERATURE + OSR;
   }
+  ms561101ba_ctx.state++;
+  i2c_MS561101BA_UT_or_UP_Read(rawValPointer);     // get the 24bit resulting from a UP of UT command request. Nothing interresting for the first cycle because we don't initiate a command in Baro_init()
+  i2c_MS561101BA_UT_or_UP_Start(commandRegister);  // send the next command to get UP or UT value after at least 8.5ms
+  return 1;
 }
 #endif
 
